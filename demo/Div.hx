9792d2d8535961b31343f70180e568cd411de48c
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-03-27 12:03:35 -0600

    updated to remove vchild

--- demo/Div.hx
+++ demo/Div.hx
@@ -2,7 +2,7 @@ import Helpers.*;
 
 import doom.html.Html.*;
 
-class Div extends doom.html.Component<{ n : doom.core.VChild }> {
+class Div extends doom.html.Component<{ n : doom.core.VNode }> {
   override function render()
     return div(props.n);
 }
